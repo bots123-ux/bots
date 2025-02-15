b65a0bc079d5dc20c9bd3a336a7199565b8dcd39
bots123-ux <rezdelacruz6@gmail.com>
2025-02-15 21:35:52 +0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const envelopeFront = document.querySelector('.envelope-front');
-const envelopeBack = document.querySelector('.envelope-back');
-
-envelopeFront.addEventListener('click', () => {
-    envelopeFront.style.display = 'none';
-    envelopeBack.style.display = 'block';
-});
